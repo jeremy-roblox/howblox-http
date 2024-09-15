860020a7b9f567a44d9ee544b112a68577feef16
jeremy-roblox <garethroberts243@gmail.com>
2024-09-15 15:32:14 +0100

    commit

--- src/bot.py
+++ src/bot.py
@@ -5,4 +5,13 @@ from os import environ as environ
 
 import hikari
 import uvicorn
-from
+from howblox_lib import load_modules, execute_deferred_module_functions, get_environment, Environment
+from howblox_lib.database import redis
+
+from resources.howblox import howblox
+from config import CONFIG
+
+# Load a few modules
+from resources.commands import handle_interaction, sync_commands
+from resources.constants import MODULES
+from web.webserver import application
